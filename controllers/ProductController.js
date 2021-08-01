6f0418f041f87b1e068ea5a85d07d576c4b4ca14
mubashir-dev <mubashirmobi633@gmail.com>
2021-08-01 21:17:58 +0500

    Validation and Postman  Collection have been Added

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,7 @@
+const { body, validationResult } = require("express-validator");
 const Product = require('../models/product.model');
 const mongoose = require('mongoose');
+
 
 
 exports.findAll = async (req, res) => {
@@ -26,44 +28,86 @@ exports.find = async (req, res) => {
     }
 };
 
-exports.create = async (req, res, next) => {
+exports.create = [
+    body("price", "Price must not be empty.").isLength({ min: 1 }).trim(),
+    body("description", "Description must not be empty.").isLength({ min: 1 }).trim(),
+    body("title", "Title must not be empty.").isLength({ min: 1 }).trim().custom((value, { req }) => {
+        return Product.findOne({ title: req.body.title }).then(product => {
+            if (product) {
+                return Promise.reject("Product already exist with this Title.");
+            }
+        });
+    }),
+    async function (req, res, next) {
 
-    try {
-        const product = new Product(req.body);
-        const result = await product.save();
-        res.send(result);
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(500);
-        res.send(error.message);
-    }
-};
+        try {
+            const errors = validationResult(req);
+            const product = new Product(req.body);
+            if (!errors.isEmpty()) {
+                let _errors = [];
+                errors.array().forEach((element) => {
+                    _errors.push(element.msg);
+                });
+                res.send({ errors: _errors });
+            }
+            else {
+                const result = await product.save();
+                res.send(result);
+            }
 
-
-exports.update = async (req, res) => {
-
-    try {
-        const foundProduct = await Product.findOne({ _id: req.params.id })
-        if (!foundProduct) {
-            res.status(404);
-            res.send({
-                error: 'Product not found'
-            });
         }
-        else {
-            const result = await Product.findByIdAndUpdate(req.params.id, req.body);
-            res.send({ message: 'Product has been updated successfully', data: result });
+        catch (error) {
+            console.log(error.message);
+            res.status(500);
+            res.send(error.message);
         }
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(500);
-        res.send(error.message);
-    }
+    },
+]
 
-};
 
+exports.update = [
+    body("price", "Price must not be empty.").isLength({ min: 1 }).trim(),
+    body("description", "Description must not be empty.").isLength({ min: 10 }).trim(),
+    body("title", "Title must not be empty.").isLength({ min: 1 }).trim().custom((value, { req }) => {
+        return Product.findOne({ title: req.body.title }).then(product => {
+            if (product) {
+                return Promise.reject("Product already exist with this Title.");
+            }
+        });
+    }),
+    async function (req, res) {
+
+        try {
+            const foundProduct = await Product.findOne({ _id: req.params.id })
+            if (!foundProduct) {
+                res.status(404);
+                res.send({
+                    message: 'Product not found with this ID'
+                });
+            }
+            else {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    let _errors = [];
+                    errors.array().forEach((element) => {
+                        _errors.push(element.msg);
+                    });
+                    res.send({ errors: _errors });
+                }
+                else {
+                    const result = await Product.findByIdAndUpdate(req.params.id, req.body);
+                    res.send({ message: 'Product has been updated successfully', data: result });
+                }
+
+            }
+        }
+        catch (error) {
+            console.log(error.message);
+            res.status(500);
+            res.send(error.message);
+        }
+    },
+]
 exports.delete = async (req, res) => {
 
     try {
